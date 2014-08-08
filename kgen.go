35a6ea0dd6647d274a7da66065c82c08da537d5f
Max Hille <mh@lambdasoup.com>
2014-08-08 11:42:01 +0200

    displaying images in index

--- kgen.go
+++ kgen.go
@@ -3,11 +3,23 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"io/ioutil"
+	"strings"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// read dir, only take jpg
+	fis, _ := ioutil.ReadDir("./img/")
+	jpgs := make([]string, 0)
+	for _, fi := range fis {
+		if !strings.HasSuffix(fi.Name(),"jpg") {
+			continue
+		}
+		jpgs = append(jpgs, fi.Name())
+	}
+
 	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, nil)
+	t.Execute(w, jpgs)
 }
 
 func main() {
